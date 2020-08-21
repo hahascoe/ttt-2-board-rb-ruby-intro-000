6f58fbca2b47ba36840050e813614c3ede9c85b9
Harrison Hascoe <hascoeharrison@gmail.com>
2020-08-21 16:49:38 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,3 @@
 # Define the variable board below.
-row1=[" "," "," "]
-row2=[" "," "," "]
-row3=[" "," "," "]
-baord= [row1[0,1,2],row2[3,4,5],row3[5,7,8]]
+row=[" "," "," "," "," "," "," "," "," "]
+baord= row
